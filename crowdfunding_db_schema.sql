4815e2b56247056b99c5692e90fe5b7d0d56b482
j13s3 <jalsha.13@gmail.com>
2023-05-22 21:33:27 +1000

    SQL Stuff

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -4,29 +4,6 @@
 
 -- This is the Entity Relationship Diagram (ERD) text for the Crowdfunding Data
 
-CREATE TABLE Campaign (
-    cf_id INT NOT NULL,
-    contact_id INT NOT NULL,
-    company_name VARCHAR NOT NULL,
-    description VARCHAR NOT NULL,
-    goal INT NOT NULL,
-    pledged INT NOT NULL,
-    outcome VARCHAR NOT NULL,
-    backers_count INT NOT NULL,
-    country VARCHAR NOT NULL,
-    currency VARCHAR NOT NULL,
-    launched_date VARCHAR NOT NULL,
-    end_date VARCHAR NOT NULL,
-    staff_pick VARCHAR NOT NULL,
-    spotlight VARCHAR NOT NULL,
-    category_id VARCHAR NOT NULL,
-    subcategory_id VARCHAR NOT NULL,
-    PRIMARY KEY (cf_id),
-    FOREIGN KEY (contact_id) REFERENCES Contacts (contact_id),
-    FOREIGN KEY (category_id) REFERENCES Category (category_id),
-    FOREIGN KEY (subcategory_id) REFERENCES Subcategory (subcategory_id)
-);
-
 CREATE TABLE Contacts (
     contact_id INT NOT NULL,
     first_name VARCHAR NOT NULL,
@@ -45,4 +22,27 @@ CREATE TABLE Subcategory (
     subcategory_id VARCHAR   NOT NULL,
     subcategory VARCHAR   NOT NULL,
     PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE Campaign (
+    cf_id INT NOT NULL,
+    contact_id INT NOT NULL,
+    company_name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    goal VARCHAR NOT NULL,
+    pledged VARCHAR NOT NULL,
+    outcome VARCHAR NOT NULL,
+    backers_count INT NOT NULL,
+    country VARCHAR NOT NULL,
+    currency VARCHAR NOT NULL,
+    launched_date VARCHAR NOT NULL,
+    end_date VARCHAR NOT NULL,
+    staff_pick VARCHAR NOT NULL,
+    spotlight VARCHAR NOT NULL,
+    category_id VARCHAR NOT NULL,
+    subcategory_id VARCHAR NOT NULL,
+    PRIMARY KEY (cf_id),
+    FOREIGN KEY (contact_id) REFERENCES Contacts (contact_id),
+    FOREIGN KEY (category_id) REFERENCES Category (category_id),
+    FOREIGN KEY (subcategory_id) REFERENCES Subcategory (subcategory_id)
 );
